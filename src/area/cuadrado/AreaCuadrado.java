a39d3ec4e18bfac24cf75e12c5685f89cc5b20e9
Daniel Felipe Camargo <camargo_daniel@hotmail.es>
2018-02-08 07:41:38 -0500

    ac

--- src/area/cuadrado/AreaCuadrado.java
+++ src/area/cuadrado/AreaCuadrado.java
@@ -17,13 +17,27 @@ public class AreaCuadrado {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Digite el lado del cuadrado");
+        System.out.println("¿Qué desea saber?");
+        System.out.println("1. Area");
+        System.out.println("2. Perimetro");
         int a;
         int s;
         Scanner sc = new Scanner(System.in);
         a = sc.nextInt();
-        s = a*a;
-        System.out.println("El area del cuadrado es: "+s);
+        switch (a){
+            case 1:
+                System.out.println("Digíte el lado del cuadrado");
+                s = sc.nextInt();
+                s = s*s;
+                System.out.println("El area del cuadrado es: "+s);
+                break;
+            case 2:
+                System.out.println("Digíte el lado del cuadrado");
+                s = sc.nextInt();
+                s = s*4;
+                System.out.println("El perimetro del cuadrado es: "+s);
+                break;
+        }
         // TODO code application logic here
     }
     
